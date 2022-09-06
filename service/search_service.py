a8d1747bc92452610c4242f28e3753babc31111a
jiangxicheng <jiangxicheng@msunsoft.com>
2022-09-06 10:53:05 +0800

    取消调试打印

--- service/search_service.py
+++ service/search_service.py
@@ -5,7 +5,7 @@ import pandas as pd
 base_dir = dirname(dirname(abspath(__file__)))
 excel_path = base_dir + "/res/题库.xlsx"
 df = pd.read_excel(excel_path, sheet_name='题库')
-print(df)
+
 
 def search_text(text):
     return df.query(f'name.str.contains("{text}")')
